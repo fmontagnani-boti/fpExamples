d8bb593ba79a24129613f4d6ceeed82c69abc8d9
fmontagnani-boti <122371213+fmontagnani-boti@users.noreply.github.com>
2023-09-29 17:09:26 -0300

    refactor: updated FP logger example to make all functions idempotent

--- src/loggerFP.ts
+++ src/loggerFP.ts
@@ -1,21 +1,24 @@
-import { LogLevels, Formatter } from "./types"
+import { LogLevels, Formatter, ILogMessage } from "./types"
 
-const formatter: Formatter = (message: string, logLevel: LogLevels) => `[${new Date().toISOString()}] [${logLevel}] ${message}`
+const formatter: Formatter = (logMessage: ILogMessage) => {
+  const { message, logDate, logLevel } = logMessage
+  return `[${logDate.toISOString()}] [${logLevel}] ${message}` 
+}
 
-const makeLevelLogger = (frmt: Formatter, logLevel: LogLevels) => (message: string) => { console.log(frmt(message, logLevel)) }
+const makeLevelLogger = (frmt: Formatter, logLevel: LogLevels) => (message: string, logDate: Date) => { console.log(frmt({ message, logLevel, logDate })) }
 
 const getLogger = (formatter: Formatter) => ({
-  info(message: string) { console.log(formatter(message, 'INFO')) },
-  error(message: string) { console.log(formatter(message, 'ERROR')) }
+  info(message: string, logDate: Date) { console.log(formatter({ message, logDate, logLevel: 'INFO' })) },
+  error(message: string, logDate: Date) { console.log(formatter({ message, logDate, logLevel: 'ERROR' })) }
 })
 
 const logInfo = makeLevelLogger(formatter, 'INFO')
 const logError = makeLevelLogger(formatter, 'ERROR')
 
-logInfo('logInfo: deu bom')
-logError('logError: deu ruim')
+logInfo('logInfo: deu bom', new Date())
+logError('logError: deu ruim', new Date())
 
 const logger = getLogger(formatter)
 
-logger.info('logger.info: Tbm Deu Bom')
-logger.error('logger.error: Tbm Deu ruim')
+logger.info('logger.info: Tbm Deu Bom', new Date())
+logger.error('logger.error: Tbm Deu ruim', new Date())
