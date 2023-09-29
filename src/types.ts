eb6d0a1e4487afa1859f33f2bbd222405a575202
fmontagnani-boti <122371213+fmontagnani-boti@users.noreply.github.com>
2023-09-29 17:08:24 -0300

    feat: added LogMessage interface

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,8 @@
 export type LogLevels = 'DEBUG' | 'INFO' | 'WARNING' | 'ERROR' | 'CRITICAL'
 export type Formatter = (message: string, logLevel: LogLevels) => string
+
+export interface ILogMessage {
+  message: string
+  logDate: Date
+  logLevel: LogLevels
+}
